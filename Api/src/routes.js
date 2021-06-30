252369adf2c4682ad725d1a9b26b134d6d6e0dc3
Felipe Friedrich Santos <54379678+FelipeFriedrich@users.noreply.github.com>
2021-06-30 17:38:46 -0300

    Retirado o Swagger

--- Api/src/routes.js
+++ Api/src/routes.js
@@ -1,10 +1,8 @@
 const  routes  = require('express').Router();
 const gitHubController = require('./controllers/githubController');
-const swagger = require('../docs/index');
 
 routes.get('/', (request, response) => { return response.json('Funcionando') });
 
 routes.post('/SearchRepos', gitHubController.SearchRepos)
 
-swagger.ui(routes);
 module.exports = routes;
